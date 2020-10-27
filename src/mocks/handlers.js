e0b007a6c4de84ff89ee72a52b099b007308a7ad
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2020-10-27 10:46:12 +0100

    Rename mock folder

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,39 +1,41 @@
 import { rest } from 'msw';
 
-import classgroupLesson from './dummyData/classgroupLesson';
-import classgroupStudents from './dummyData/classgroupStudents';
-import lesson from './dummyData/lesson';
-import lessonDetails from './dummyData/lessonDetails';
-import studentInfo from './dummyData/studentInfo';
+import {
+  classgroupDetails,
+  classgroupLesson,
+  classgroupStudents,
+} from './mockData/classgroup';
+import lessonDetails from './mockData/lessonDetails';
+import studentInfo from './mockData/studentInfo';
 
 export const handlers = [
   rest.get(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/classgroups/:id`,
+    (_, res, ctx) => {
+      return res(ctx.json(classgroupDetails));
+    },
+  ),
+  rest.get(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/classgroups/:classgroupId/lessons/:lessonId/students`,
-    (req, res, ctx) => {
+    (_, res, ctx) => {
       return res(ctx.json(studentInfo));
     },
   ),
   rest.get(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/classgroups/:classgroupId/students`,
-    (req, res, ctx) => {
+    (_, res, ctx) => {
       return res(ctx.json(classgroupStudents));
     },
   ),
   rest.get(
-    `${process.env.NEXT_PUBLIC_BASE_URL}/api/classgroups/:classgroupId/lessons/:id`,
-    (req, res, ctx) => {
-      return res(ctx.json(lesson));
-    },
-  ),
-  rest.get(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/lessons/:id`,
-    (req, res, ctx) => {
+    (_, res, ctx) => {
       return res(ctx.json(lessonDetails));
     },
   ),
   rest.get(
     `${process.env.NEXT_PUBLIC_BASE_URL}/api/classgroups/:classgroupId/lessons/:lessonId}`,
-    (req, res, ctx) => {
+    (_, res, ctx) => {
       return res(ctx.json(classgroupLesson));
     },
   ),
---
+++ src/mocks/mockData/classgroup.js
@@ -0,0 +1,25 @@
+import faker from 'faker';
+
+export const classgroupDetails = {
+  name: '1A',
+};
+
+export const classgroupLesson = {
+  id: 'd49515ed-972c-11e9-b935-0a0034000161',
+  publishStatus: 'OPEN_FOR_HOME',
+  updatedAt: '2019-01-01T00:00:00.000+0100',
+  resultsPublished: false,
+  givenOn: null,
+  givenBy: null,
+};
+
+export const classgroupStudents = Array.from(Array(20), (_, i) => ({
+  id: i,
+  username: faker.internet.userName(),
+  firstName: faker.name.firstName(),
+  lastName: faker.name.lastName(),
+  lastLogin: faker.date.past().toISOString(),
+  idp: 'FTRPRF',
+  role: 'SCHOOLSTUDENT',
+  schoolId: faker.random.uuid(),
+}));
