b7ca2374af74bdbae829b53d4bb373172fe6335d
Tijl <tijl.ivens@gmail.com>
2020-10-27 15:50:16 +0100

    add snapshot

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -4,5 +4,6 @@ import TeacherHeader from './Header';
 describe('Teacher header', () => {
   it('should render without crashing', () => {
     const { container } = render(<TeacherHeader />);
+    expect(container).toMatchSnapshot();
   });
 });
