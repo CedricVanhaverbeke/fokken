b06a42bd817afae71dc67118f9c3e79e6e011669
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2020-10-28 17:07:20 +0100

    Demian is boos

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+dotenv.config({ path: '.env.test' });
 // eslint-disable-next-line no-unused-vars
 import next from 'next';
 import { cache } from 'swr';
@@ -8,7 +9,6 @@ import '@testing-library/jest-dom/extend-expect';
 import { server } from '../mocks/server';
 
 beforeAll(() => {
-  dotenv.config({ path: '.env.test' });
   server.listen({ onUnhandledRequest: 'error' });
 });
 
