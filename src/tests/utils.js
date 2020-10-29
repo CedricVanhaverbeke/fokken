b58aeb823d76d426b29c613532ec01cceb093869
Demian Dekoninck <demian@live.be>
2020-10-29 14:52:30 +0100

    add a mockRouter

--- src/tests/utils.js
+++ src/tests/utils.js
@@ -1,6 +1,32 @@
 import { render as rtlRender } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { getPage } from 'next-page-tester';
+import { RouterContext } from 'next/dist/next-server/lib/router-context';
+import { SWRConfig } from 'swr';
+
+import { browserFetcher } from '@/utils/fetcher';
+
+const mockRouter = {
+  basePath: '',
+  pathname: '/classgroups/[classGroupId]/lessons',
+  route: '/classgroups/[classGroupId]/lessons',
+  asPath: '/classgroups/d49515ed-972c-11e9-b935-0a0027000063/lessons',
+  query: {
+    classGroupId: 'd49515ed-972c-11e9-b935-0a0027000063',
+  },
+  push: jest.fn(),
+  replace: jest.fn(),
+  reload: jest.fn(),
+  back: jest.fn(),
+  prefetch: jest.fn(),
+  beforePopState: jest.fn(),
+  events: {
+    on: jest.fn(),
+    off: jest.fn(),
+    emit: jest.fn(),
+  },
+  isFallback: false,
+};
 
 const mockPageRouter = () => {
   const push = jest.fn().mockImplementation(async () => {});
@@ -14,8 +40,20 @@ const mockPageRouter = () => {
   };
 };
 
-export const render = (ui) => {
-  return { ...rtlRender(ui), userEvent };
+export const render = (children, { router } = {}) => {
+  return {
+    ...rtlRender(
+      <RouterContext.Provider value={{ ...mockRouter, ...router }}>
+        <SWRConfig
+          value={{
+            fetcher: browserFetcher,
+          }}
+        >
+          {children}
+        </SWRConfig>
+      </RouterContext.Provider>,
+    ),
+  };
 };
 
 export const renderPage = async (route) => {
@@ -26,5 +64,5 @@ export const renderPage = async (route) => {
     router: () => router,
   });
 
-  return { ...render(Page), router };
+  return { ...rtlRender(Page), router, userEvent };
 };
