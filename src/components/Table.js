790f9417bfbc37424a54c8bb18aef1d7e2fb2e7d
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2020-11-10 14:59:54 +0100

    Untracked files

---
+++ src/components/Table.js
@@ -0,0 +1,94 @@
+import React, { useMemo } from 'react';
+import { useTable } from 'react-table';
+
+import c from '@/utils/c';
+
+const TableHeader = ({ headerGroups, headerClassName, columnClassName }) => (
+  <thead className="rounded-t-md">
+    {headerGroups.map((headerGroup) => (
+      <tr
+        key={headerGroup}
+        {...headerGroup.getHeaderGroupProps([{ className: headerClassName }])}
+      >
+        {headerGroup.headers.map((column) => (
+          <th
+            key={column}
+            {...column.getHeaderProps([
+              {
+                className: columnClassName,
+              },
+            ])}
+          >
+            {column.render('Header')}
+          </th>
+        ))}
+      </tr>
+    ))}
+  </thead>
+);
+
+const Table = ({
+  columns,
+  data,
+  className,
+  columnClassName,
+  rowClassName,
+  cellClassName,
+  headerClassName,
+}) => {
+  const memoizedData = useMemo(() => data || Array(30).fill({}), [data]);
+  const memoizedColumns = useMemo(
+    () =>
+      data
+        ? columns
+        : columns.map(({ Skeleton, ...columnProps }) => ({
+            ...columnProps,
+            Cell: Skeleton ? (
+              <Skeleton />
+            ) : (
+              <div className="bg-gray-200 animate-pulse h-10 rounded" />
+            ),
+          })),
+    [columns, data],
+  );
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({
+    columns: memoizedColumns,
+    data: memoizedData,
+  });
+
+  return (
+    <table className={c(className)} {...getTableProps()}>
+      <TableHeader
+        headerGroups={headerGroups}
+        headerClassName={headerClassName}
+        columnClassName={columnClassName}
+      />
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr key={row} className={rowClassName} {...row.getRowProps()}>
+              {row.cells.map((cell) => (
+                <td
+                  key={cell}
+                  {...cell.getCellProps([{ className: cellClassName }])}
+                >
+                  {cell.render('Cell')}
+                </td>
+              ))}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
---
+++ src/components/pages/StudentAnswers/Skeleton.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import c from '@/utils/c';
+
+const Skeleton = () => (
+  <div className="flex flex-col w-full">
+    {Array.from(Array(20), (_, i) => (
+      <div className="flex w-full justify-center" key={i}>
+        <div className="w-full flex flex-col sm:flex-row">
+          <span className="flex-shrink-0 h-8 w-32 bg-gray-200 animate-pulse mb-2" />
+          <div className="w-full flex flex-col items-center flex-grow">
+            <div className="max-w-6xl w-full">
+              <div
+                className={c(
+                  'bg-gray-200 animate-pulse rounded mb-4',
+                  i % 2 === 0 ? 'h-32' : 'h-24',
+                )}
+              />
+              {i % 2 === 0 && (
+                <div className="grid grid-cols-2 gap-2 w-full mb-4">
+                  {Array.from(
+                    Array(Math.floor((Math.random() * 10) % 5)),
+                    () => (
+                      <div className="h-12 bg-gray-200 rounded animate-pulse" />
+                    ),
+                  )}
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
+
+export default Skeleton;
