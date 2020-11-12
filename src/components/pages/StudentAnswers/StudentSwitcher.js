74f83abfdf4f6841c1bf47cfef347ec5e27c298b
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2020-11-12 16:19:36 +0100

    Added dropdown

--- src/components/pages/StudentAnswers/StudentSwitcher.js
+++ src/components/pages/StudentAnswers/StudentSwitcher.js
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { useRouter } from 'next/router';
 
 import useClassGroupLessonStudents from '@/hooks/api/useClassGroupLessonStudents';
 
-import c from '@/utils/c';
-
-import Link from '@/components/Link';
-
 const StudentSwitcher = () => {
   const router = useRouter();
-  const { classGroupId, lessonId, studentId, viewMode } = router.query;
+  const {
+    classGroupId,
+    lessonId,
+    studentId: currentStudentId,
+    viewMode,
+  } = router.query;
 
   const { classGroupLessonStudents, isLoading } = useClassGroupLessonStudents(
     classGroupId,
     lessonId,
   );
 
-  const submittedStudents = classGroupLessonStudents.filter(
-    (s) => !!s.submittedAt,
-  );
+  const navigateToStudent = (studentId) => {
+    router.push({
+      pathname: `/classgroups/${classGroupId}/lessons/${lessonId}/students/${studentId}`,
+      query: { viewMode: viewMode },
+    });
+  };
 
-  const studentIndex = classGroupLessonStudents?.findIndex(
-    (student) => student.id === studentId,
+  const submittedStudents = useMemo(() => {
+    if (!classGroupLessonStudents) {
+      return null;
+    }
+
+    return classGroupLessonStudents?.filter((s) => !!s.submittedAt);
+  }, [classGroupLessonStudents]);
+
+  const studentIndex = submittedStudents?.findIndex(
+    (student) => student.id === currentStudentId,
   );
-  const currentStudent = submittedStudents?.[studentIndex];
 
   if (isLoading) {
     return <div className="bg-gray-300 w-32 h-8 animate-pulse rounded" />;
@@ -32,42 +43,48 @@ const StudentSwitcher = () => {
 
   return (
     <div className="flex gap-x-2 items-center text-center">
-      <span>{`${currentStudent.firstName} ${currentStudent.lastName}`}</span>
+      <select
+        style={{ textAlignLast: 'right' }}
+        className="flex-grow h-8 bg-gray-100 text-right pr-2 border-none outline-none"
+        value={currentStudentId}
+        onChange={({ target }) => navigateToStudent(target.value)}
+      >
+        {submittedStudents.map((student) => (
+          <option
+            key={student.id}
+            value={student.id}
+            className="bg-white border-white outline-white"
+          >
+            {`${student.firstName} ${student.lastName}`}
+          </option>
+        ))}
+      </select>
+
       <div className="flex">
-        <Link
-          href={{
-            pathname: `/classgroups/${classGroupId}/lessons/${lessonId}/students/${
-              submittedStudents[studentIndex - 1]?.id
-            }`,
-            query: { viewMode: viewMode },
-          }}
+        <button
+          onClick={() =>
+            navigateToStudent(submittedStudents[studentIndex - 1]?.id)
+          }
           disabled={studentIndex === 0}
         >
           <FaAngleLeft
-            className={c(
-              'text-black',
-              studentIndex === 0 && 'text-gray-500 cursor-not-allowed',
-            )}
+            className={studentIndex === 0 && 'text-gray-500 cursor-not-allowed'}
           />
-        </Link>
+        </button>
 
-        <Link
-          href={{
-            pathname: `/classgroups/${classGroupId}/lessons/${lessonId}/students/${
-              submittedStudents[studentIndex + 1]?.id
-            }`,
-            query: { viewMode: viewMode },
-          }}
-          disabled={studentIndex === classGroupLessonStudents.length - 1}
+        <button
+          onClick={() =>
+            navigateToStudent(submittedStudents[studentIndex + 1]?.id)
+          }
+          disabled={studentIndex === submittedStudents.length - 1}
         >
           <FaAngleRight
-            className={c(
-              'text-black',
-              studentIndex === classGroupLessonStudents.length - 1 &&
-                'text-gray-500 cursor-not-allowed',
-            )}
+            className={
+              studentIndex === submittedStudents.length - 1 &&
+              'text-gray-500 cursor-not-allowed'
+            }
           />
-        </Link>
+        </button>
       </div>
     </div>
   );
