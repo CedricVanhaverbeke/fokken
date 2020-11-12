065099a3133d5d12dd3919cfa103443ec2494393
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2020-11-12 16:24:31 +0100

    Add tests

---
+++ src/components/pages/StudentAnswers/StudentSwitcher.test.js
@@ -0,0 +1,7 @@
+import { render } from '@/tests/utils';
+
+jest.mock(('next/router', () => {}));
+
+describe('StudentSwitcher', () => {
+  it('Should show the student with the id in the query params', () => {});
+});
