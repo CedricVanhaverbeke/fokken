78214b5aede286a5386c73254bf521ef386c30e7
Demian Dekoninck <demian@live.be>
2020-11-13 12:31:49 +0100

    fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
+import { EMPTY_PROPS } from '@/utils/constants';
+
 const Home = () => {
   return null;
 };
 
 export function getServerSideProps({ res }) {
   res.writeHead(302, { Location: process.env.NEXT_PUBLIC_EDU_URL }).end();
+
+  return EMPTY_PROPS;
 }
 
 export default Home;
