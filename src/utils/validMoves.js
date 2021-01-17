911d06a900d7247308f347b74868f6db170079b5
Cedric Vanhaverbeke <cedric.vanhaverbeke@hotmail.com>
2021-01-17 16:47:37 +0100

    give ace correct priority and make it possible to play the same card

--- src/utils/validMoves.js
+++ src/utils/validMoves.js
@@ -1,12 +1,14 @@
 // There are essentially two rules:
-// Always play higher cards
-// You can lay any number of cards you want.
+// Always play higher or equal cards
+// You can play any number of cards you want.
 // This function returns an array of the cards you can put on the stack. These cards will be enabled in your hand
 
 // Function receives a card with a number. Only the number is important
 
 const specialCards = [2, 9, 10];
 const HIGHEST_CARD = 13;
+const LOWEST_CARD = 2;
+const ACE = 1;
 
 const symbolMapper = {
   J: 11,
@@ -22,11 +24,13 @@ const numberMapper = {
 
 const validMoves = (playedCards) => {
   if (playedCards.length === 0) {
+    // return every card when there are no playedCards yet
     return [
       ...new Set([
+        ACE,
         ...Array.from(
-          Array(HIGHEST_CARD - 0),
-          (_, i) => numberMapper[i + 1] || i + 1,
+          Array(HIGHEST_CARD - 1),
+          (_, i) => numberMapper[i + LOWEST_CARD] || i + LOWEST_CARD,
         ),
         ...specialCards,
       ]),
@@ -36,10 +40,15 @@ const validMoves = (playedCards) => {
   let { number } = playedCards[playedCards.length - 1];
   const previousCard = playedCards[playedCards.length - 2];
 
-  const previousNumber = previousCard ? previousCard.number : 0;
+  const previousNumber = previousCard ? previousCard.number : 1;
 
   if (number === 7) {
-    return [...new Set([1, 2, 3, 4, 5, 6, 7, ...specialCards])];
+    return [...new Set([2, 3, 4, 5, 6, 7, ...specialCards])];
+  }
+
+  // ace
+  if (number === 1) {
+    return [ACE, ...specialCards];
   }
 
   if (number === 9) {
@@ -50,10 +59,10 @@ const validMoves = (playedCards) => {
 
   return [
     ...new Set([
+      ACE,
       ...Array.from(
-        Array(HIGHEST_CARD - numberOrSymbol),
-        (_, i) =>
-          numberMapper[i + 1 + numberOrSymbol] || i + 1 + numberOrSymbol,
+        Array(HIGHEST_CARD - numberOrSymbol + 1),
+        (_, i) => numberMapper[i + numberOrSymbol] || i + numberOrSymbol,
       ),
       ...specialCards,
     ]),
--- src/utils/validMoves.test.js
+++ src/utils/validMoves.test.js
@@ -2,8 +2,9 @@ import validMoves from './validMoves';
 
 describe('validMoves', () => {
   it('should show all cards', () => {
-    expect(validMoves([])).toMatchObject([
+    expect(validMoves([]).sort()).toMatchObject([
       1,
+      10,
       2,
       3,
       4,
@@ -12,132 +13,16 @@ describe('validMoves', () => {
       7,
       8,
       9,
-      10,
       'J',
-      'Q',
       'K',
+      'Q',
     ]);
   });
 
   it('should show the correct cards', () => {
-    expect(validMoves([{ number: 1 }])).toMatchObject([
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      'J',
-      'Q',
-      'K',
-    ]);
-  });
-
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 2 }])).toMatchObject([
-      3,
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      'J',
-      'Q',
-      'K',
-      2, // special card
-    ]);
-  });
-
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 3 }])).toMatchObject([
-      4,
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      'J',
-      'Q',
-      'K',
-      2, // special card
-    ]);
-  });
-
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 4 }])).toMatchObject([
-      5,
-      6,
-      7,
-      8,
-      9,
-      10,
-      'J',
-      'Q',
-      'K',
-      2, // special card
-    ]);
-  });
-
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 5 }])).toMatchObject([
-      6,
-      7,
-      8,
-      9,
-      10,
-      'J',
-      'Q',
-      'K',
-      2, // special card
-    ]);
-  });
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 6 }])).toMatchObject([
-      7,
-      8,
-      9,
-      10,
-      'J',
-      'Q',
-      'K',
-      2, // special card
-    ]);
-  });
-
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 7 }])).toMatchObject([
+    expect(validMoves([{ number: 2 }]).sort()).toMatchObject([
       1,
-      2,
-      3,
-      4,
-      5,
-      6,
-      7,
-      9, // spcial card
-      10, // special card
-    ]);
-  });
-
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 8 }])).toMatchObject([
-      9,
       10,
-      'J',
-      'Q',
-      'K',
-      2, // special card
-    ]);
-  });
-
-  it('should show the correct cards', () => {
-    expect(validMoves([{ number: 9 }])).toMatchObject([
-      1,
       2,
       3,
       4,
@@ -146,75 +31,207 @@ describe('validMoves', () => {
       7,
       8,
       9,
+      'J',
+      'K',
+      'Q',
+    ]);
+  });
+
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 3 }]).sort()).toMatchObject([
+      1,
       10,
+      2, // special card
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
       'J',
-      'Q',
       'K',
-    ]);
-
-    expect(validMoves([{ number: 'K' }, { number: 9 }])).toMatchObject([
-      2, // special card
-      9, // spcial card
-      10, // special card
+      'Q',
     ]);
   });
 
   it('should show the correct cards', () => {
-    expect(validMoves([{ number: 10 }])).toMatchObject([
+    expect(validMoves([{ number: 4 }]).sort()).toMatchObject([
+      1,
+      10,
+      2, // special card
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
       'J',
-      'Q',
       'K',
-      2, // special card
-      9, // spcial card
-      10, // special card
+      'Q',
     ]);
   });
 
   it('should show the correct cards', () => {
-    expect(validMoves([{ number: 'J' }])).toMatchObject([
-      'Q',
-      'K',
+    expect(validMoves([{ number: 5 }]).sort()).toMatchObject([
+      1,
+      10,
       2, // special card
-      9, // spcial card
-      10, // special card
+      5,
+      6,
+      7,
+      8,
+      9,
+      'J',
+      'K',
+      'Q',
     ]);
-
-    expect(validMoves([{ number: 11 }])).toMatchObject([
-      'Q',
-      'K',
+  });
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 6 }]).sort()).toMatchObject([
+      1,
+      10,
       2, // special card
-      9, // spcial card
-      10, // special card
+      6,
+      7,
+      8,
+      9,
+      'J',
+      'K',
+      'Q',
     ]);
   });
 
   it('should show the correct cards', () => {
-    expect(validMoves([{ number: 'Q' }])).toMatchObject([
-      'K',
-      2, // special card
-      9, // spcial card
+    expect(validMoves([{ number: 7 }]).sort()).toMatchObject([
       10, // special card
-    ]);
-
-    expect(validMoves([{ number: 12 }])).toMatchObject([
-      'K',
-      2, // special card
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
       9, // spcial card
-      10, // special card
     ]);
   });
 
   it('should show the correct cards', () => {
-    expect(validMoves([{ number: 'K' }])).toMatchObject([
+    expect(validMoves([{ number: 8 }]).sort()).toMatchObject([
+      1,
+      10,
       2, // special card
-      9, // spcial card
-      10, // special card
+      8,
+      9,
+      'J',
+      'K',
+      'Q',
+    ]);
+  });
+
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 9 }]).sort()).toMatchObject([
+      1,
+      10,
+      2,
+      3,
+      4,
+      5,
+      6,
+      7,
+      8,
+      9,
+      'J',
+      'K',
+      'Q',
     ]);
 
-    expect(validMoves([{ number: 13 }])).toMatchObject([
+    expect(validMoves([{ number: 'K' }, { number: 9 }]).sort()).toMatchObject([
+      1,
+      10, // special card
       2, // special card
       9, // spcial card
+      'K',
+    ]);
+  });
+
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 10 }]).sort()).toMatchObject([
+      1,
       10, // special card
+      2, // special card
+      9, // spcial card
+      'J',
+      'K',
+      'Q',
+    ]);
+  });
+
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 'J' }]).sort()).toMatchObject([
+      1,
+      10, // special card
+      2, // special card
+      9, // spcial card
+      'J',
+      'K',
+      'Q',
+    ]);
+
+    expect(validMoves([{ number: 11 }]).sort()).toMatchObject([
+      1,
+      10, // special card,
+      2, // special card
+      9, // spcial card
+      'J',
+      'K',
+      'Q',
+    ]);
+  });
+
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 'Q' }]).sort()).toMatchObject([
+      1,
+      10, // special card
+      2, // special card
+      9, // spcial card
+      'K',
+      'Q',
+    ]);
+
+    expect(validMoves([{ number: 12 }]).sort()).toMatchObject([
+      1,
+      10, // special card
+      2, // special card
+      9, // spcial card
+      'K',
+      'Q',
+    ]);
+  });
+
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 'K' }]).sort()).toMatchObject([
+      1, // ace
+      10, // special card
+      2, // special card
+      9, // spcial card
+      'K',
+    ]);
+
+    expect(validMoves([{ number: 13 }]).sort()).toMatchObject([
+      1, // ace
+      10, // special card
+      2, // special card
+      9, // spcial card
+      'K',
+    ]);
+  });
+
+  it('should show the correct cards', () => {
+    expect(validMoves([{ number: 1 }]).sort()).toMatchObject([
+      1,
+      10, // special card
+      2, // special card
+      9, // spcial card
     ]);
   });
 });
